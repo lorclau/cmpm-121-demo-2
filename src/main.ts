4439c80c2308cb390a3a78b53438e78562b95f7b
lorclau <lorclau38@gmail.com>
2024-10-22 20:48:55 -0700

    Step 1: complete - initial UI layout

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,16 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// Create the app title
+const appTitle = document.createElement('h1');
+appTitle.textContent = 'The Scribble App';
+app.append(appTitle);
+
+// Create the canvas
+const canvas = document.createElement('canvas');
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+
+
